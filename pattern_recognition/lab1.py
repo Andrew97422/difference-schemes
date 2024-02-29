8032c4ac721885c6c2697a4a5f474240fc332cb9
Andrew <99014710+Andrew97422@users.noreply.github.com>
2024-02-29 14:52:29 +0300

    Update lab1.py

--- pattern_recognition/lab1.py
+++ pattern_recognition/lab1.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-#  Функция для вычисления расстояния
-def ro(X, Z):
-    return np.sqrt(np.sum((X - Z) ** 2))
-
-
 #  Разделяющая граница
 def d(x, Z1, Z2):
     return (-(Z1[0] - Z2[0]) * x + (Z1[0] ** 2 + Z1[1] ** 2 - Z2[0] ** 2 - Z2[1] ** 2) / 2) / (Z1[1] - Z2[1])
+
+
+#  Критерий классификации
+def d_func(X, Z1, Z2):
+    return (Z1[0] -Z2[0]) * X[0] + (Z1[1] - Z2[1]) * X[1] + (Z2[0] ** 2 + Z2[1] ** 2 - Z1[0] ** 2 - Z1[1] ** 2) / 2
 
 
 W1 = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
@@ -25,15 +25,16 @@ plt.scatter(Z1[0], Z1[1], c='b', s=75)
 plt.scatter(Z2[0], Z2[1], c='r', s=75)
 x = np.arange(0, 11, 0.01)
 plt.scatter(x, d(x, Z1, Z2), c='black', s=5)
+y = np.arange(0, 11, 0.01)
+for i in range(len(x)):
+    y[i] = d(x[i], Z1, Z2)
 
 round = 1e-3
 
-if ro(X, Z2) - ro(X, Z1) > round:
+if d_func(X, Z1, Z2) > 0:
     plt.scatter(X[0], X[1], c='b', s=30)
-elif ro(X, Z1) - ro(X, Z2) > round:
-    plt.scatter(X[0], X[1], c='r', s=30)
 else:
-    plt.scatter(X[0], X[1], c='black', s=30)
+    plt.scatter(X[0], X[1], c='r', s=30)
 
 plt.xlim(-1, 12)
 plt.ylim(-1, 6)
